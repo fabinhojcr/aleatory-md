11a30498c2fb388982fdd26f88c2a3d2d60ecaa7
fabinhojcr <fabinhojcr@hotmail.com>
2022-11-18 07:52:27 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ GREEN='\033[0;32m'
 while : 
 do
 echo "${GREEN} Fαbiи_Bot-md _ Auto reconexão ativada para prevenção de quedas.."
-    node iniciar.js
+    node --trace-warnings iniciar.js
     sleep 1
 
 done
